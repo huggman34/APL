338f2b0f3bedec9675b2b6bb456cfe80244a9cfd
George1025 <70201225+George1025@users.noreply.github.com>
2021-03-09 14:32:26 +0100

    dag filer kommenterade

--- dag/editdag.php
+++ dag/editdag.php
@@ -5,8 +5,13 @@
     <title>Document</title>
 </head>
 <body>
-    
-    <?php 
+    <?php
+    /**
+     * Denna filen används för att kunna uppdatera dagar i dag tabellen i databasen
+     * Du väljer vilken dag som ska uppdateras med hjälp av tabellen som finns i 'dagredigering.php'
+     * som visar alla dagar i dag tabellen.
+     * Sedan så väljer man vad den ska uppdateras till och skickar in det.
+     */
 
         session_start();
         require_once 'connection.php';
