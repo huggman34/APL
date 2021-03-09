4c16dc40110e7746e1411d691ae92a8c39800c37
George1025 <70201225+George1025@users.noreply.github.com>
2021-03-09 11:02:48 +0100

    kommenterad connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,9 @@
 <?php 
+/**
+ * Denna filen tar servernamn, användarnamn, lösenord och vilken databas.
+ * Sedan som gör den kopplingen via mysqli tilläget till vår lokala mysql databas.
+ * Denna filen kommer att inkluderas i alla filer som behöver kommunicera med databasen.
+ */
     $servername = 'localhost';
     $username = 'root';
     $password = '';
