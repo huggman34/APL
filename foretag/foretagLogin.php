e49ac70634654c9eb2a7845d0ef5e188ce089023
George1025 <70201225+George1025@users.noreply.github.com>
2021-03-09 11:28:55 +0100

    företag filer i företag mapp

--- foretag/foretagLogin.php
+++ foretag/foretagLogin.php
@@ -1,4 +1,11 @@
-<?php 
+<?php
+/**
+ * Denna filen används så att Företag kan logga in.
+ * Den hämtar datan som skrivs in i login formuläret för företag.
+ * Sedan så kollar den om namn och lösenord finns i företag tabellen i databasen.
+ * Om det stämmer så skickas du vidare.
+ * Men om det inte gör det så får du skriva om inloggnings uppgifter.
+ */
   session_start();
   include_once "connection.php";
 
