33b27ad47ee6b10034a5b52acba0d84c9bd3ef44
Boelmeister <joel.backstrand@gmail.com>
2021-03-12 10:35:46 +0100

    Formlinks

---
+++ registerLinks.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Vad vill du registrera?</h2>
+    <a href="elev/elev.php">Elever</a><br>
+    <br>
+    <a href="foretag/foretag.php">Företag</a><br>
+    <br>
+    <a href="perioddag/perioddag.php">Dagar</a><br>
+    <br>
+    <a href="period/periodCreate.php">Perioder</a><br>
+    <br>
+    <a href="plats.php">Plats</a><br>
+    <br>
+    <a href="narvaro/reportform.php">Närvaro</a><br>
+</body>
+</html>
