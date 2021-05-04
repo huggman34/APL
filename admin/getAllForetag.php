9f4c3c367fdbf5b773c6c19e44cbb916016c4b9d
George1025 <70201225+George1025@users.noreply.github.com>
2021-05-04 14:40:05 +0200

    all get files commented

--- admin/getAllForetag.php
+++ admin/getAllForetag.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Denna filen används för att kunna uppdatera select options som ska visa alla företag även nya utan
+ * att behöva uppdatera webbsidan med hjälp av AJAX request. filen hämtar alla företag i företag tabellen
+ * i databasen genom foretag funktionen från ViewFunctions.php och gör om det till
+ * JSON format som sedan lägger till datan som option tagar i relevanta select tagar
+ */
     require_once '../connection.php';
     require_once "../ViewFunctions.php";
 
