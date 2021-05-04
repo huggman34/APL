231c320ce07cebbb39fd5cdf54190c9b3ccb7fc7
George1025 <70201225+George1025@users.noreply.github.com>
2021-05-04 10:53:30 +0200

    clean files

--- admin/periodSelect.php
+++ admin/periodSelect.php
@@ -4,57 +4,38 @@
     require_once '../ViewFunctions.php';
     require_once '../UpdateFunctions.php';
 
-$platsPeriod=$_POST['platsPeriod'];
+    $platsPeriod=$_POST['platsPeriod'];
 
-function selectPeriod($conn,$platsPeriod,$elevID){
-$sql = "SELECT * FROM elev INNER JOIN plats ON elev.elevID=plats.elevID
-WHERE plats.periodNamn=? AND plats.elevID=?";
+    function selectPeriod($conn,$platsPeriod,$elevID) {
+        $sql = "SELECT * FROM elev INNER JOIN plats ON elev.elevID=plats.elevID
+        WHERE plats.periodNamn=? AND plats.elevID=?";
 
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $platsPeriod,$elevID);
-$stmt->execute();
-$result = $stmt->get_result();
-$data = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ss", $platsPeriod,$elevID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $data = $result->fetch_all(MYSQLI_ASSOC);
 
-return $data;
-}
+        return $data;
+    }
     
     $elev=selectTabel($conn,"elev");
-    /*$klass=selectTabel($conn,"klass");
-    echo'<select id="platsKlass" onchange="elevPlatsPeriod();">';
-    echo "<option disabled selected> Välj klass </option>";
-    foreach ($klass as $kls) {
-        $kl=$kls['klass'];
-        echo"<option value='$kl'>$kl</option>";
-    }
-    echo"</select>";
-    echo "<table>";
-    echo "<thead><tr><th>Elev</th><th>Klass</th></tr></thead><tbody>";*/
 
-    
     foreach ($elev as $e) {
         if (isset($_POST['platsKlass'])) {
-                $platsKlass=$_POST['platsKlass'];
-                $elevID=$e['elevID'];
+            $platsKlass=$_POST['platsKlass'];
+            $elevID=$e['elevID'];
 
-                $data=selectPeriod($conn,$platsPeriod,$elevID);
+            $data=selectPeriod($conn,$platsPeriod,$elevID);
 
-        if (empty($data) && $platsKlass==$e['klass']) {
-
-        
-        echo "<tr><td>";
-        echo $e['elevID'];
-        echo "</td><td>";
-        //echo $e['klass'];
-        echo "</td><td>";
-        echo "<input id='elevPeriod' type='checkbox' name='elevPeriod' value='$elevID'>";
-        echo "</td></tr>";
+            if (empty($data) && $platsKlass==$e['klass']) {
+                echo "<tr><td>";
+                echo $e['elevID'];
+                echo "</td><td>";
+                echo "<input id='elevPeriod' type='checkbox' name='elevPeriod' value='$elevID'>";
+                echo "</td></tr>";
+            }
         }
-        
-        }
-
     }
     echo "</tbody></table>";
-
-
 ?>
--- admin/regPeriod.php
+++ admin/regPeriod.php
@@ -8,56 +8,37 @@
         $slutdatum=$_POST['slutdatum'];
     }
 
- //if (isset($_POST['submin'])) {
-    
-    
-        if (isset($_POST['submin'])) {
-            periodGeneration($conn,$_POST['periodnamn'],$startdatum,$slutdatum,$_POST['periodDag']);
-        }else{
-            
-        //echo $periodNamn;
+    if (isset($_POST['submin'])) {
+        periodGeneration($conn,$_POST['periodnamn'],$startdatum,$slutdatum,$_POST['periodDag']);
+    } else {
+
         echo "<table class='dagTable'>";
         echo "<thead><tr><th>Vecka</th><th>Dag</th><th>Datum</th><th></th></tr></thead><tbody>";
         $start=strtotime($startdatum);
         $slut=strtotime($slutdatum);
         $dagar=ceil(($slut-$start)/60/60/24);
         
-        for ($i=0; $i < $dagar+1; $i++) { 
+        for ($i=0; $i < $dagar+1; $i++) {
            
-           $gto=strtotime("+$i days",$start);
-           $datum=date('Y-m-d',$gto);
+            $gto=strtotime("+$i days",$start);
+            $datum=date('Y-m-d',$gto);
 
-           
-           echo "<tr><td>";
-           echo date('W',$gto);
-           echo "</td><td>";
-           echo date('l',$gto);
-           echo "</td><td>";
-           echo $datum;
-           echo "</td><td>";
-           if (("Saturday"==date("l",$gto)) || ("Sunday"==date("l",$gto))) {
-            echo"<input id='periodDag' type='checkbox' name='periodDag' value='$datum'>";
-        }else {
-            echo"<input id='periodDag' type='checkbox' name='periodDag' value='$datum' checked>";
+            echo "<tr><td>";
+            echo date('W',$gto);
+            echo "</td><td>";
+            echo date('l',$gto);
+            echo "</td><td>";
+            echo $datum;
+            echo "</td><td>";
+
+            if (("Saturday"==date("l",$gto)) || ("Sunday"==date("l",$gto))) {
+                echo"<input id='periodDag' type='checkbox' name='periodDag' value='$datum'>";
+
+            } else {
+                echo"<input id='periodDag' type='checkbox' name='periodDag' value='$datum' checked>";
+            }
+            echo"</td></tr>";
         }
-           echo"</td></tr>";
-       }
-       
-       //echo'<input type="submit" value="submit" id="submin">';
-       echo "</tbody></table>";
-     
-    /*echo"<form method='post'>
-    <input type='submit' name='submit' value='börja om'>
-    </form>";*/
+        echo "</tbody></table>";
     }
-    
-
-    /*if(isset($_POST['periodnamn'], $_POST['slutdatum'], $_POST['startdatum'],$_POST['perioddag'])) {
-        $periodNamn = $_POST['periodnamn'];
-        $slutdatum = $_POST['slutdatum'];
-        $startdatum = $_POST['startdatum'];
-        $periodDag = $_POST['perioddag'];
-     onclick=\"return confirm('Är du säker?');\"
-        periodGeneration($conn, $periodNamn, $startdatum, $slutdatum, $periodDag);
-    }*/
 ?>
